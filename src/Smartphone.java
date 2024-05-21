2763d6f74b39cfe956bfbdbd991a79ab441c7783
Vinicius Aristimunha <viniciusarisp@gmail.com>
2024-05-21 13:27:50 -0300

    feat: Add Smartphone class implementing interfaces for music playback, phone calls, and internet browsing

---
+++ src/Smartphone.java
@@ -0,0 +1,50 @@
+import navegador.NavegadorInternet;
+import reprodutor.ReprodutorMusical;
+import telefone.AparelhoTelefonico;
+
+public class Smartphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página");
+    }
+}
